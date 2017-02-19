7844cd573feb3bf29c3d274fc6fc24d7772c155c
Riku Lääkkölä <riku.laakkola@iki.fi>
2017-02-19 10:09:58 +0200

    table layout

--- src/components/departurelist.js
+++ src/components/departurelist.js
@@ -2,37 +2,49 @@ import React from 'react';
 import { graphql } from 'react-apollo';
 import gql from 'graphql-tag';
 import moment from 'moment';
+import { Table } from 'react-bootstrap';
 
 // import * as Actions from '../actions';
 
 const DepartureList = (props) => {
   const formatTime = tstamp => moment.unix(tstamp).format('HH:mm');
-  const list = [];
 
-  if (!props.data.loading) {
-    props.data.stops.map(stop =>
-      stop.stoptimesForPatterns.map(departure =>
-        list.push(
-          {
-            stop: departure.stoptimes[0].stop.name,
-            line: departure.pattern.name,
-            time: departure.stoptimes[0].serviceDay + departure.stoptimes[0].realtimeDeparture,
-            isRealtime: departure.stoptimes[0].realtime,
-          },
+  const list = props.data.loading
+    ? []
+    : props.data.stops.reduce((acc, stop) =>
+        acc.concat(
+          stop.stoptimesForPatterns.map(departure =>
+            ({
+              stop: departure.stoptimes[0].stop.name,
+              line: departure.pattern.name,
+              time: departure.stoptimes[0].serviceDay + departure.stoptimes[0].realtimeDeparture,
+              isRealtime: departure.stoptimes[0].realtime,
+            }),
+          ),
         ),
-      ),
-    );
-  }
+        [],
+      );
+
   list.sort((a, b) => a.time - b.time);
   const rows = list.map((departure, i) =>
-    <div key={i}>
-      {departure.stop} {departure.line} {formatTime(departure.time)} {departure.isRealtime ? 'gps!' : ''}
-    </div>,
+    <tr key={i}>
+      <td>
+        {departure.stop}
+      </td>
+      <td>
+        {departure.line}
+      </td>
+      <td>
+        {formatTime(departure.time)} {departure.isRealtime ? <i className="fa fa-rss" aria-hidden="true" /> : ''}
+      </td>
+    </tr>,
   );
   return (
-    <div>
-      {rows}
-    </div>
+    <Table>
+      <tbody>
+        {rows}
+      </tbody>
+    </Table>
   );
 };
 
